9101f8ed4cd98630b896d5005b7efffba7338145
Marina <marina.zhurbina25@gmail.com>
2024-09-30 19:26:48 +0300

    The folder structure has been changed

--- lab1/generation_test_10.py
+++ lab1/generation_test_10.py
@@ -4,6 +4,6 @@ n = 100000
 ord_A = ord('A')
 ord_Z = ord('Z')
 s = [chr(random.randint(ord_A, ord_Z)) for i in range(n)]
-with open('input_10.txt', 'wt') as f:
+with open('Task10/input_10.txt', 'wt') as f:
     f.write(str(n) + '\n')
     f.write(''.join(s))
